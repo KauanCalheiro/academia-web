3b50adce38f8a9edfbbdc46bf80b01a40f78355e
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-08-31 09:36:38 -0300

    chore: Update VS Code settings and editor configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@nuxt/ui"],
+
+    modules: [
+        "@nuxt/ui",
+    ],
+
     css: [
         '~/assets/css/fonts.css',
     ],
+
     future: {
         compatibilityVersion: 4,
-    }
+    },
+
+    compatibilityDate: '2024-08-31'
 })
