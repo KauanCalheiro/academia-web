d1fd2d740c8403d1d3e6d2fe5215a76445b5c2d1
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-08-31 10:42:35 -0300

    feat: Add landing page with background image and logo

---
+++ app/pages/pagina-captura/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: "clean-layout",
+});
+</script>
+
+<template>
+    <main>
+        <section
+            class="bg-[url('https://www.academiazenith.com.br/images/7078118458ec29316db719d9225ad107.jpg')] bg-center bg-no-repeat bg-cover h-[80vh] flex flex-col justify-between pb-[35vh] sm:pb-[30vh] lg:pb-[25vh]"
+        >
+            <div class="flex items-center justify-start">
+                <img
+                    class="w-[50vw] sm:w-[45vw] md:w-[40vw] lg:w-[35vw] xl:w-[30vw] 2xl:w-[25vw] m-4 p-2 filter drop-shadow-[3px_2px_1px_#000]"
+                    src="~~/public/logo.png"
+                    alt="Logo"
+                />
+            </div>
+            <div>
+                <h1
+                    class="text-[11vw] sm:text-[10vw] md:text-[9vw] lg:text-[8vw] xl:text-[7vw] 2xl:text-[6vw] main-font font-bold italic text-white text-center drop-shadow-[3px_3px_2px_#000] uppercase m-0 p-4"
+                >
+                    Reach Your <span class="text-lime-400">Best</span>
+                </h1>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+.main-font {
+    font-family: "Oswald", sans-serif;
+}
+</style>
