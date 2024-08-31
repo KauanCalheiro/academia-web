43042d8ae6097e84acc290f1b8641a926ee7d2d2
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-08-30 22:00:30 -0300

    feat: add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
-  future:{
-    compatibilityVersion: 4,
-  }
+    devtools: { enabled: true },
+    modules: ["@nuxt/ui"],
+    css: [
+        '~/assets/css/fonts.css',
+    ],
+    future: {
+        compatibilityVersion: 4,
+    }
 })
