afeb4ac9045a1a9876ffd5566991da69206f98b7
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-09-03 20:44:55 -0300

    feat: Add CatchPageCard component for displaying catch page content

---
+++ app/components/CatchPageCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <section
+        :class="`bg-[url('${imgPath}')] bg-center bg-cover bg-no-repeat h-[50dvh] w-[80dvw] md:w-[25dvw]
+        flex flex-col justify-between`"
+    >
+        <slot> </slot>
+    </section>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    backgroundImageRef: string;
+}>();
+
+const imgPath = ref<string>(props.backgroundImageRef);
+
+imgPath.value =
+    "https://www.academiazenith.com.br/images/7078118458ec29316db719d9225ad107.jpg";
+</script>
+
+<style scoped></style>
