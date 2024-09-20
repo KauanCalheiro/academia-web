1353280c1f4fdf7e643464c3495e0d51e350ab66
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-09-20 14:40:47 -0300

    feat: update api endpoints

--- constants/api.ts
+++ constants/api.ts
@@ -1,3 +1,2 @@
-const API_BASE_URL = 'http://177.44.248.110:8000/api';
-
-export const LOGIN = `${API_BASE_URL}/login`;
+export const ZENITH_API_BASE_ENDPOINT = 'http://177.44.248.110:8000/api';
+export const LOGIN_ENDPOINT = `${ZENITH_API_BASE_ENDPOINT}/login`;
