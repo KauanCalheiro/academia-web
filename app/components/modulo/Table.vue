c8f7088825d20cdbf6e8f6a7d92eab2fd944fced
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-09-20 22:29:20 -0300

    feat: Adicionar componente Tabela e TabelaSkeleton (working)

---
+++ app/components/modulo/Table.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import type ZenithApiResponse from "~~/types/api/ZenithApiResponse";
+import type Modulo from "~~/types/Modulo";
+import type TableColumn from "~~/types/ui/TableColumn";
+import type TableSort from "~~/types/ui/TableSort";
+
+const columns: TableColumn[] = [
+    { label: "ID", key: "id", sortable: true },
+    { label: "Módulo", key: "descricao", sortable: true },
+    { label: "Caminho", key: "path", sortable: true },
+];
+
+const sortRequest = ref<TableSort>({
+    column: "id",
+    direction: "asc",
+});
+
+const order = computed(() => {
+    return sortRequest.value.column;
+});
+
+const direction = computed(() => {
+    return sortRequest.value.direction;
+});
+
+const { data, status, error, refresh, clear, execute } = useLazyFetch<
+    ZenithApiResponse<Modulo>
+>("/api/module", {
+    params: {
+        offset: 0,
+        limit: 10,
+        order: order,
+        direction: direction,
+    },
+    watch: [order, direction],
+});
+
+function onUpdateSort(sort: TableSort) {
+    sortRequest.value = sort;
+}
+</script>
+
+<template>
+    <h1>Cadastros Básicos</h1>
+
+    <BaseTable
+        :columns="columns"
+        :rows="data?.payload ?? []"
+        :status="status"
+        :error="error?.message"
+        @update:sort="onUpdateSort"
+    ></BaseTable>
+    {{ data }}
+</template>
