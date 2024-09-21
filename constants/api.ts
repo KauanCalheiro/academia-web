d3b1118840f3e0eeed5136769dc163edb14577b0
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-09-20 22:28:23 -0300

    feat: add module endpoint

--- constants/api.ts
+++ constants/api.ts
@@ -1,2 +1,4 @@
-export const ZENITH_API_BASE_ENDPOINT = 'http://177.44.248.110:8000/api';
+export const ZENITH_API_BASE_ENDPOINT = 'http://127.0.0.1:8000/api';
+
 export const LOGIN_ENDPOINT = `${ZENITH_API_BASE_ENDPOINT}/login`;
+export const MODULE_ENDPOINT = `${ZENITH_API_BASE_ENDPOINT}/modulo`;
