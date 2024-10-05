54dd5714312cd419bbb74c32527fac5b5e7e48e9
Kauan Morinel Calheiro <kauan.calheiro@universo.univates.br>
2024-10-05 14:25:17 -0300

    feat: Adicionar componente SideMenu

---
+++ app/components/base/SideMenu.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const isOpen = ref(false);
+</script>
+
+<template>
+    <UButton
+        icon="material-symbols:menu-rounded"
+        size="md"
+        @click="isOpen = true"
+        color="white"
+        variant="soft"
+    />
+
+    <USlideover v-model="isOpen">
+        <UCard>
+            <template #header>
+                <div class="flex flex-row justify-end">
+                    <UButton
+                        icon="material-symbols:close-rounded"
+                        size="sm"
+                        color="gray"
+                        variant="ghost"
+                        @click="isOpen = false"
+                    />
+                </div>
+            </template>
+        </UCard>
+    </USlideover>
+</template>
