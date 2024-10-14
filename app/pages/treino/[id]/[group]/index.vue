932f19c335736f64fd1e5e91006a6aa0fa403a7b
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-10-14 11:55:52 -0300

    feat: adiciona tela de exercicios do treino

---
+++ app/pages/treino/[id]/[group]/index.vue
@@ -0,0 +1,22 @@
+<template>
+    {{ data }}
+</template>
+
+<script setup lang="ts">
+// get id and group from route
+
+import { useRoute } from "vue-router";
+import type ZenithApiResponse from "~~/types/api/ZenithApiResponse";
+import type ExercicioTreino from "~~/types/ExercicioTreino";
+
+const route = useRoute();
+
+const id = route.params.id;
+const group = route.params.group;
+
+const { data, error, status } = await useLazyFetch<
+    ZenithApiResponse<ExercicioTreino[]>
+>(`/api/workout/${id}/${group}`, {
+    method: "GET",
+});
+</script>
